95219488363149b5cafccf8973064c650d92c571
SK <102964001+skdev2@users.noreply.github.com>
2025-07-24 22:34:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+    const banner = document.getElementById("cookieBanner");
+    const accepted = localStorage.getItem("cookiesAccepted");
+
+    if (!accepted) {
+        banner.style.display = "flex";
+    }
+};
+
+function acceptCookies() {
+    localStorage.setItem("cookiesAccepted", "true");
+    document.getElementById("cookieText").innerHTML =
+        "Du har tilladt cookies – tak! <a href='cookie-policy.html'>Læs mere her.</a>";
+    setTimeout(() => document.getElementById("cookieBanner").style.display = "none", 3000);
+}
+
+function declineCookies() {
+    localStorage.setItem("cookiesAccepted", "false");
+    document.getElementById("cookieText").innerHTML =
+        "Du har afvist cookies. <a href='cookie-policy.html'>Læs om dine muligheder her.</a>";
+    setTimeout(() => document.getElementById("cookieBanner").style.display = "none", 3000);
+}
